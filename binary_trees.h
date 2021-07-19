98c17f7ca01b7278474129d35f38e6c196e39ef7
theurikenneth <theurikenneth@gmail.com>
2021-07-19 07:24:43 +0000

    C - Binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -21,9 +21,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-/*Binary search tree*/
+/*Binary Search Tree*/
 typedef struct binary_tree_s bst_t;
-/*AVL tree*/
+/*AVL Tree*/
 typedef struct binary_tree_s avl_t;
 /*Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
