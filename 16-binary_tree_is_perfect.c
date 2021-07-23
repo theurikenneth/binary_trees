b53a3902862524fa0c56357734594942947368f1
theurikenneth <theurikenneth@gmail.com>
2021-07-23 16:05:26 +0000

    C - Binary trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,52 +8,38 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+/* if tree is null return 0 */
 if (!tree)
 return (0);
+/* if one node, it's perfect */
 if (!tree->left && !tree->right)
 return (1);
-if (tree->left && tree->right)
-if (binary_tree_height(tree->left) ==
-binary_tree_height(tree->right))
-return (binary_tree_is_perfect(tree->left) &&
-binary_tree_is_perfect(tree->right));
+/* if different heights it's not perfect */
+if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+return (0);
+/* if all it satisfied on recursive calls */
+if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+return (1);
+/* the compiler made me add this */
 return (0);
 }
-
+/**
+ * binary_tree_height - get the height of tree
+ * @tree: the tree to measure
+ *Return: size_t height or 0
+ *
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t levelr = 0, levell = 0;
+/* declarations */
+size_t lh, rh;
 
-if (!tree)
+/* tree is null or a loner */
+if (!tree || (!tree->left && !tree->right))
 return (0);
-if (tree->lef)
-{
-levell = binary_tree_height(tree->left);
-levelr += 1;
-}
-if (tree->right)
-{
-levelr = binary_tree_height(tree->right);
-levelr += 1;
-}
-return (levelr > levell ? levelr : levell);
-}
-
-/**
- * binary_tree_is_full - Function that checks if a binary tree is full
- * @tree: Root node iof the tree to check
- *
- * Return: 1 if is full, otherwise 0
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-if (!tree)
-return (0);
-
-if (!tree->left && !tree->right)
-return (1);
-if (tree->left && tree->right)
-return (binary_tree_is_full(tree->right) &&
-binary_tree_is_full(tree->left));
-return (0);
+/* measure both */
+lh = binary_tree_height(tree->left);
+rh = binary_tree_height(tree->right);
+/* compare and return tallest */
+return ((lh > rh ? lh : rh) + 1);
 }
