75ad28f182cfead12b04eb7633fad20510c4c612
theurikenneth <theurikenneth@gmail.com>
2021-07-23 15:50:05 +0000

    C - Binary trees

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -13,9 +13,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 if (!tree || !func)
 return;
 
-if (tree->left)
 binary_tree_inorder(tree->left, func);
-(func)(tree->n);
-if (tree->right)
+func(tree->n);
 binary_tree_inorder(tree->right, func);
 }
