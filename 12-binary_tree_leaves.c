68f2f4084adc915f79659828721c1fbb7ea464c5
theurikenneth <theurikenneth@gmail.com>
2021-07-23 15:56:18 +0000

    C - Binary trees

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,16 +8,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-size_t leaf = 0;
-
 if (!tree)
 return (0);
 
-if (tree->left)
-leaf += binary_tree_leaves(tree->left);
-if (tree->right)
-leaf += binary_tree_leaves(tree->right);
 if (!tree->left && !tree->right)
-leaf += 1;
-return (leaf);
+return (1);
+
+return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
